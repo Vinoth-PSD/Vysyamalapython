910a49598ee37ef34289ed44f2881f207faf4b06
Vinoth-PSD <156505851+Vinoth-PSD@users.noreply.github.com>
2025-03-31 20:18:18 +0530

    Add files via upload

--- authentication/urls.py
+++ authentication/urls.py
@@ -217,6 +217,13 @@ urlpatterns = [
      path("profile/<str:profile_id>/", views.profile_preview, name="profile_preview"),
      path("profile_view/<str:profile_id>/", views.profile_preview_withouphoto, name="profile_preview"),
 
+
+     path('generate-porutham-pdf/', views.generate_porutham_pdf, name='generate_porutham_pdf'),
+      
+     path('My_horoscope_pdf_color/<str:user_profile_id>/',  views.My_horoscope_generate, name='horoscope_pdf'),
+
+     path('My_horoscope_black/<str:user_profile_id>/',  views.My_horoscope, name='horoscope_pdf'),
+
     # path('generate-porutham-pdf/', views.generate_porutham_pdf, name='generate_porutham_pdf'),
     
     # path('My_horoscope_pdf_color/<str:user_profile_id>/',  views.My_horoscope_generate, name='horoscope_pdf'),
