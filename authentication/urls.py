e8e6ba23502f426f89ad19445c51320ed109efe8
Vinoth-PSD <vinoth@psdigitise.com>
2025-04-04 11:57:17 +0530

    update

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views 
-from .views import WithoutAddressSendEmailAPI,HoroscopeKattamAPI
+# from .views import WithoutAddressSendEmailAPI,HoroscopeKattamAPI
 
 
 urlpatterns = [
