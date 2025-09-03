580f6a1c0235db35547a6a64138d5adc98e4a566
Vinoth-PSD <156505851+Vinoth-PSD@users.noreply.github.com>
2025-09-03 13:08:20 +0530

    ChevvaiFixed

--- authentication/models.py
+++ authentication/models.py
@@ -1215,7 +1215,7 @@ class Get_profiledata(models.Model):
 
                     base_query += " AND (LOWER(e.calc_chevvai_dhosham) = 'yes' OR LOWER(e.calc_chevvai_dhosham) = 'True' OR e.calc_chevvai_dhosham = '1' OR e.calc_chevvai_dhosham = 1 OR e.calc_chevvai_dhosham IS NULL OR e.calc_chevvai_dhosham ='')"
                 elif partner_pref_chevvai and partner_pref_chevvai.lower() == 'no':
-                    base_query += "  AND (LOWER(e.calc_chevvai_dhosham) = 'no' OR LOWER(e.calc_chevvai_dhosham) = 'False' OR e.calc_raguketu_dhosham = '2' OR e.calc_chevvai_dhosham = 2 OR e.calc_chevvai_dhosham IS NULL OR e.calc_chevvai_dhosham ='')"
+                    base_query += "  AND (LOWER(e.calc_chevvai_dhosham) = 'no' OR LOWER(e.calc_chevvai_dhosham) = 'False' OR e.calc_chevvai_dhosham = '2' OR e.calc_chevvai_dhosham = 2 OR e.calc_chevvai_dhosham IS NULL OR e.calc_chevvai_dhosham ='')"
 
                 height_conditions = ""
                 if partner_pref_height_from and partner_pref_height_to:
